4b010adbcc7571b2d5e4afb271adac112f96cc57
Spwilson87 <simonpeterwilson87@gmail.com>
2022-01-16 17:47:33 +0000

    added db cursur commands

--- webapp/app.py
+++ webapp/app.py
@@ -40,22 +40,42 @@ def platform():
 @app.route('/add_publisher')
 def publisher():
     return render_template("add_publisher.html")
-
-@app.route('/done', methods = ['POST', 'GET'])
+#use add_games form to insert data to database
+@app.route('/add_games_done', methods = ['POST', 'GET'])
 def login():
     if request.method == 'GET':
         return "error"
      
     if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        age = request.form['age']
+        game_id = request.form['game_id']
+        game_name = request.form['game_name']
+        release_year = request.form['release_year']
+        pub_id = request.form['pub_id']
+        dev_id = request.form['dev_id']
+        platform_id = request.form['platform_id']
+        genre_id = request.form['genre_id']
         cursor = mysql.connection.cursor()
         cursor.execute("USE database_games")
-        cursor.execute(''' INSERT INTO people VALUES(%s,%s,%s)''',(first_name, last_name, age))
+        cursor.execute(''' INSERT INTO games VALUES(%s,%s,%s,%s,%s,%s,%s)''',(game_id,game_name, release_year, pub_id, dev_id, platform_id, genre_id))
         mysql.connection.commit()
         cursor.close()
         return render_template("done.html")
+# use add_genre form to insert data to database
+# @app.route('/add_genre_done', methods = ['POST', 'GET'])
+# def login():
+#     if request.method == 'GET':
+#         return "error"
+     
+#     if request.method == 'POST':
+#         first_name = request.form['first_name']
+#         last_name = request.form['last_name']
+#         age = request.form['age']
+#         cursor = mysql.connection.cursor()
+#         cursor.execute("USE database_games")
+#         cursor.execute(''' INSERT INTO people VALUES(%s,%s,%s)''',(first_name, last_name, age))
+#         mysql.connection.commit()
+#         cursor.close()
+#         return render_template("done.html")
 
 
 if __name__ == '__main__':
