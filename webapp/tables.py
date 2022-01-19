2f8854f5eb98f91c845fe054a75125f8516c4cda
Spwilson87 <simonpeterwilson87@gmail.com>
2022-01-19 12:02:51 +0000

    added routes for wishlist, playing and completed

--- webapp/tables.py
+++ webapp/tables.py
@@ -15,3 +15,27 @@ def owned():
     cursor.execute(''' Select * From Games_Owned ''')
     owned = cursor.fetchall()
     return render_template('owned_table.html', title='Owned Games', owned=owned)
+
+@table.route('/wishlist_table')
+def wishlist():
+    cursor = mysql.connection.cursor()
+    cursor.execute("USE database_games")
+    cursor.execute(''' Select * From Games_Wishlist ''')
+    wish = cursor.fetchall()
+    return render_template('wishlist_table.html', title='Wishlist', wish=wish)
+
+@table.route('/playing_table')
+def playing():
+    cursor = mysql.connection.cursor()
+    cursor.execute("USE database_games")
+    cursor.execute(''' Select * From Games_Playing ''')
+    play = cursor.fetchall()
+    return render_template('playing_table.html', title='Currently Playing', play=play)
+
+@table.route('/completed_table')
+def completed():
+    cursor = mysql.connection.cursor()
+    cursor.execute("USE database_games")
+    cursor.execute(''' Select * From Games_Completed ''')
+    comp = cursor.fetchall()
+    return render_template('completed_table.html', title='Completed Games', comp=comp)
