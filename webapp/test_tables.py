56dcb7e1f0c4781a569deb9e469248b6cc557cf1
Spwilson87 <simonpeterwilson87@gmail.com>
2022-01-20 18:25:39 +0000

    wrote test for inside jenkins

--- webapp/test_tables.py
+++ webapp/test_tables.py
@@ -5,11 +5,17 @@ import unittest
 class FlaskTestCase(unittest.TestCase):
 
     #check for html response 200
-    def test_home(self):
+    def test_owned_table(self):
         tester = app.test_client(self)
         response = tester.get("/owned_table")
-        statuscode = response.status_code
-        self.assertEqual(statuscode, 200)
+        self.assertTrue(b'game_id' in response.data)
+        self.assertTrue(b'game_name' in response.data)
+        self.assertTrue(b'release_year' in response.data)
+        self.assertTrue(b'genre' in response.data)
+        self.assertTrue(b'developer' in response.data)
+        self.assertTrue(b'publisher' in response.data)
+        self.assertTrue(b'platform' in response.data)
+        self.assertTrue(b'Enter Game ID to Add to Playing Games' in response.data)
 
             
 
